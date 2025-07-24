54ab7af3811f2ae31a9fb8a856bd03a4c3043d37
Mateus Chaves <mateus.chavito@hotmail.com>
2025-07-24 16:06:30 -0300

    feat: Adicionando Annotations nos models

--- Horizon/Models/Quarto.cs
+++ Horizon/Models/Quarto.cs
@@ -1,23 +1,18 @@
-﻿namespace Horizon.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Horizon.Models
 {
     public class Quarto
     {
-        public int IdTiposDeQuarto { get; set; }
+        [Key]
+        public int QuartoId { get; set; }
 
-        public string? Descrição { get; set; }
-
+        public string? Descricao { get; set; }
         public bool AmbienteClimatizado { get; set; }
-
         public bool Tv { get; set; }
-
         public bool Varanda { get; set; }
-
         public bool Frigobar { get; set; }
-
         public bool Disponibilidade { get; set; }
-
         public decimal ValorDoQuarto { get; set; }
-
-
     }
 }
