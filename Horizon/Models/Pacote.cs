9e0bfea01c8abf7e9fa901494ab9fdad51d7eaa0
matheusmgs22 <matheusmgs17@gmail.com>
2025-07-24 13:49:29 -0300

    adicionando models/pacote

---
+++ Horizon/Models/Pacote.cs
@@ -0,0 +1,21 @@
+﻿namespace Horizon.Models
+{
+    public class Pacote
+    {
+        public int IdPacote { get; set; }
+
+        public required string Titulo { get; set; } 
+
+        public required string Descricao { get; set; } 
+
+        public required string Destino { get; set; } 
+
+        public int Duracao { get; set; } 
+
+        public int QuantidadeDePessoas { get; set; } 
+
+        public decimal valorTotal { get; set; } 
+
+
+    }
+}
