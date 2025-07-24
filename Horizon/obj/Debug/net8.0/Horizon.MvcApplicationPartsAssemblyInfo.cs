0cba74dfb40c9f6faab25d0a211dc8914f85cdcd
Mateus Chaves <mateus.chavito@hotmail.com>
2025-07-24 13:10:51 -0300

    TesteCommit

--- Horizon/obj/Debug/net8.0/Horizon.MvcApplicationPartsAssemblyInfo.cs
+++ Horizon/obj/Debug/net8.0/Horizon.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Horizon")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Horizon")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Horizon")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
