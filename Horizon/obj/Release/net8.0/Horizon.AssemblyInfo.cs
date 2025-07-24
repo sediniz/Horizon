7e7439e2449cc7b9cc5fde6c61b3ae3995c703e3
sediniz <mattheusd20@gmail.com>
2025-07-24 14:17:03 -0300

    coneceção com o banco

--- Horizon/obj/Release/net8.0/Horizon.AssemblyInfo.cs
+++ Horizon/obj/Release/net8.0/Horizon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Horizon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cba74dfb40c9f6faab25d0a211dc8914f85cdcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Horizon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Horizon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
