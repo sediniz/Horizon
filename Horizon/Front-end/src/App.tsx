376d2952b90f1a02331eff231bd2ed12ccf5807b
Mateus Chaves <mateus.chavito@hotmail.com>
2025-07-25 15:04:59 -0300

    feat: Subindo Front-end

---
+++ Horizon/Front-end/src/App.tsx
@@ -0,0 +1,37 @@
+import Header from './components/Header/Header';
+import Carousel from './components/Carousel/Carousel';
+import ContentBlock from './components/ContentBlock/ContentBlock';
+import EvaluationRedirection from './components/Avaliation/Avaliation';
+import Footer from './components/Footer/Footer';
+import './App.css';
+
+// Importando as imagens corretamente
+import img1 from './assets/img1.jpeg';
+import img2 from './assets/img2.png';
+import img3 from './assets/img3.png';
+
+// Fallback com URLs funcionais caso as imagens locais falhem
+const fallbackImages = [
+  'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800&h=400&fit=crop',
+  'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=800&h=400&fit=crop',
+  'https://images.unsplash.com/photo-1501594907352-04cda38ebc29?w=800&h=400&fit=crop',
+];
+
+const imageList = [
+  img1,
+  img2,
+  img3,
+];
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Carousel images={imageList} />
+      <ContentBlock />
+      <EvaluationRedirection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Horizon/Front-end/src/components/Avaliation/Avaliation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Avaliation.css';
+
+const Avaliation: React.FC = () => {
+  return (
+    <div className="Avaliation">
+      <h2>Avaliation Component</h2>
+      <p>Este é o componente Avaliation</p>
+    </div>
+  );
+};
+
+export default Avaliation;
