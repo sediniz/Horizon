2d5f6f02a1ddd608fa95468a4fbbf7db113cfbdb
Raul Vitor Melo Da Silva <raul.melo.da.silva@avanade.com>
2025-07-25 11:07:55 -0300

    feat: repository Hotel

---
+++ Horizon/Repositories/Interface/IHotelRepository.cs
@@ -0,0 +1,13 @@
+﻿using Horizon.Models;
+
+namespace Horizon.Repositories.Interface
+{
+    public interface IHotelRepository : IRepository<Hotel>
+    {
+        Task<Hotel?> GetByNameAsync(string nome);
+        Task<bool> NameExistsAsync(string nome);
+        Task<IEnumerable<Hotel>> GetHotelsByLocalizacaoAsync(string localizacao);
+        Task<IEnumerable<Hotel>> GetHotelsDisponiveisAsync(DateTime dataInicio, DateTime dataFim);
+        Task<IEnumerable<Hotel>> GetHotelsComCaracteristicasAsync(bool estacionamento, bool petFriendly, bool piscina, bool wifi);
+    }
+}
