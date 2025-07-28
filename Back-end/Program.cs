9b3ac09bc05ba24f11c33c724d423565768dd110
matheusmgs22 <matheusmgs17@gmail.com>
2025-07-28 13:21:48 -0300

    Fix: Corrigindo program

--- Back-end/Program.cs
+++ Back-end/Program.cs
@@ -1,41 +1,57 @@
 using Horizon.Data;
+
 using Horizon.Models;
+
 using Horizon.Repositories.Implementations;
+
 using Horizon.Repositories.Interface;
+
 using Horizon.Services.Implementations;
+
 using Horizon.Services.Interfaces;
+
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 // Configuração do DbContext
+
 builder.Services.AddDbContext<HorizonDbContext>(options =>
+
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Registro do repositorio
-//builder.Services.AddScoped<IService<Hotel>, HotelService>();
-builder.Services.AddScoped<IService<Quarto>, QuartoService>();
-builder.Services.AddScoped<IService<Avaliacao>, AvaliacaoService>();
-//builder.Services.AddScoped<IServicey<Reserva>, ReservaService>();
 
-// builder.Services.AddScoped<IService<Pagamento>, PagamentoService>();
-builder.Services.AddScoped<IService<Usuario>, UsuarioService>();
-// builder.Services.AddScoped<IPacote<Pacote>, PacoteService>();
+
+builder.Services.AddScoped<Horizon.Services.Interfaces.IService<Quarto>, QuartoService>();
+builder.Services.AddScoped<Horizon.Services.Interfaces.IService<Avaliacao>, AvaliacaoService>();
+builder.Services.AddScoped<Horizon.Services.Interfaces.IService<Reserva>, ReservaService>();
+builder.Services.AddScoped<Horizon.Services.Interfaces.IService<Pagamento>, PagamentoService>();
+builder.Services.AddScoped<Horizon.Services.Interfaces.IService<Usuario>, UsuarioService>();
+builder.Services.AddScoped<IPacote, PacoteService>();
 
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
+
 {
+
     app.UseSwagger();
+
     app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
@@ -45,3 +61,10 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
+
+
+
+
