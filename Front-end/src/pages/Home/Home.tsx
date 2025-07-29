137f454f646b821a4fbb2b09f538833c51278f6d
matheusmgs22 <matheusmgs17@gmail.com>
2025-07-29 19:04:50 -0300

    Feat: Adicionando Search do home

--- Front-end/src/pages/Home/Home.tsx
+++ Front-end/src/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
-import Carousel from '../../components/Carousel/Carousel';
-import ContentBlock from '../../components/ContentBlock/ContentBlock';
-import Review from '../../components/Review/Review';
+import Carousel from './Carousel/Carousel';
+import Search from './Search/Search';
+import ContentBlock from './ContentBlock/ContentBlock';
+import Review from './Review/Review';
 
 // Importando as imagens corretamente
-import img1 from '../../../assets/cancun.png';
-import img2 from '../../../assets/Paris2.png';
-import img3 from '../../../assets/Praia01.png';
+import img1 from '../../assets/cancun.png';
+import img2 from '../../assets/Paris2.png';
+import img3 from '../../assets/Praia01.png';
 
 const imageList = [
   img1,
@@ -15,10 +16,31 @@ const imageList = [
 
 function Home() {
   return (
-    <div className="Home">
-      <Carousel images={imageList} />
-      <ContentBlock />
-      <Review />
+    <div className="Home min-h-screen bg-gray-50">
+      
+      {/* Seção principal com espaçamento */}
+      <main className="space-y-16 pb-16">
+        {/* Search Component */}
+        <section className="pt-12">
+          <Search />
+        </section>
+        
+        {/* Carousel */}
+        <section>
+          <Carousel images={imageList} />
+        </section>
+        
+        {/* Content Block */}
+        <section className="px-4">
+          <ContentBlock />
+        </section>
+        
+        {/* Review Section */}
+        <section className="px-4">
+          <Review />
+        </section>
+      </main>
+      
     </div>
   );
 }
