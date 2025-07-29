f333df4bef0ddce9dce8f98e98495a5dc6a0382f
DaffodilSum6788 <lucasrobertopereirabal@gmail.com>
2025-07-29 13:37:25 -0300

    refactor: adição da página home, usando app.tsx como router

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,35 +1,12 @@
 import Header from './components/Header/Header';
-import Carousel from './components/Carousel/Carousel';
-import ContentBlock from './components/ContentBlock/ContentBlock';
-import Review from './components/Review/Review';
 import Footer from './components/Footer/Footer';
+import Home from './components/screens/Home/Home';
 import './output.css';
-
-// Importando as imagens corretamente
-import img1 from './assets/cancun.png';
-import img2 from './assets/Paris2.png';
-import img3 from './assets/Praia01.png';
-
-
-// Fallback com URLs funcionais caso as imagens locais falhem
-const fallbackImages = [
-  'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800&h=400&fit=crop',
-  'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=800&h=400&fit=crop',
-  'https://images.unsplash.com/photo-1501594907352-04cda38ebc29?w=800&h=400&fit=crop',
-];
-
-const imageList = [
-  img1,
-  img2,
-  img3,
-];
 function App() {
   return (
     <div className="App">
       <Header />
-      <Carousel images={imageList} />
-      <ContentBlock />
-      <Review />
+      <Home />
       <Footer />
     </div>
   );
