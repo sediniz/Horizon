e98ed25d0ccf20afad497ae191ff21ebd3dbfcac
adreamawaken <manoelvsantana@hotmail.com>
2025-08-01 14:35:46 -0300

    fix:Adição de ./reservas no Index.ts

--- Front-end/src/api/index.ts
+++ Front-end/src/api/index.ts
@@ -3,7 +3,7 @@ export * from './config';
 export * from './pagamento';
 export * from './pacotes';
 export * from './hoteis';
+export * from './reservas';
 
 // export * from './auth';
-// export * from './reservas';
 // export * from './usuarios';
